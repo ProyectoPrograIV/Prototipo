2721a168b4936579e64722c6a05de831b7329e48
Rafa Diaz Galeano <32416909+RafikzDiaz@users.noreply.github.com>
2018-06-12 23:00:06 -0600

    Add files via upload

---
+++ CascaronPrograIV/CascaronPrograIV/Archivos/WebForms/Ingreso.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ingreso.aspx.cs" Inherits="CascaronPrograIV.Archivos.WebForms.Ingreso" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link href="~/Archivos/HojasdeEstilos/EstiloLogin.css" type="text/css" rel="stylesheet" id="EstiloLogin" /> 
+<title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body class="body">
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <form class="login" runat="server">
+        <h1 class="login-title">Ingreso a PK2</h1>
+        <div class="auto-style1">
+        <asp:TextBox CssClass="login-input" ID="TbxUsuario" runat="server"></asp:TextBox>
+        <asp:TextBox CssClass="login-input" ID="TbxPassword" runat="server"></asp:TextBox>
+        <asp:Button CssClass="login-button" ID="BtnIngresar" runat="server" Text="Ingresar" />
+            <br />
+            <br />
+        <asp:Label CssClass="login-lost" ID="LblRegistrarse" runat="server" Text="Registrar Usuario" ForeColor="Black"></asp:Label>
+            <br />
+            <br />
+        <asp:Label CssClass="login-lost" ID="LblRecuperacion" runat="server" Text="Recuparar Datos de Ingreso" ForeColor="Black"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CascaronPrograIV/CascaronPrograIV/Archivos/WebForms/Ingreso.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CascaronPrograIV.Archivos.WebForms {
+    
+    
+    public partial class Ingreso {
+        
+        /// <summary>
+        /// Control EstiloLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink EstiloLogin;
+        
+        /// <summary>
+        /// Control TbxUsuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbxUsuario;
+        
+        /// <summary>
+        /// Control TbxPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbxPassword;
+        
+        /// <summary>
+        /// Control BtnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnIngresar;
+        
+        /// <summary>
+        /// Control LblRegistrarse.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblRegistrarse;
+        
+        /// <summary>
+        /// Control LblRecuperacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblRecuperacion;
+    }
+}
