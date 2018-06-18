b8bcd224e4bd52540f6eda3a9986dc9040a76f0a
David <David@DESKTOP-2E22NH5>
2018-06-17 18:12:15 -0600

    Cambios Interfaz

--- CascaronPrograIV/CascaronPrograIV/Archivos/WebForms/Ingreso.aspx.cs
+++ CascaronPrograIV/CascaronPrograIV/Archivos/WebForms/Ingreso.aspx.cs
@@ -13,5 +13,10 @@ namespace CascaronPrograIV.Archivos.WebForms
         {
 
         }
+
+        protected void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("PaginaPrincipal.aspx");
+        }
     }
 }
