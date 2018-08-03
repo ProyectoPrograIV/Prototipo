4a24f6cc747f30ee6c1d301ff0a1bcacae17e201
Leo <Leo@DESKTOP-B8GJTTT>
2018-08-02 21:19:25 -0600

    Construi la Carpeta de Liquidacion de Viaticos dentro de la Capa LN, dentro de la carpeta estoy iniciando el codigo de la clase LiquidacionViaticos

---
+++ CascaronPrograIV/LN/LNLiquidacionViaticos/ClsLiquidacionViaticos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AD.ADSolicitudViaticos;
+using Entidades;
+using System.Data;
+
+namespace LN.LNLiquidacionViaticos
+{
+    class ClsLiquidacionViaticos
+    {
+    }
+}
